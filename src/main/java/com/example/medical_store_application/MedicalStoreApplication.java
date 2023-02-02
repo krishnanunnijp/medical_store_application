2b58faec8e15d9fef957adb9537f14744645ff96
krishnanunnijp <krishnanunni.j@nestgroup.net>
2023-02-02 10:42:12 +0530

    Initial commit

---
+++ src/main/java/com/example/medical_store_application/MedicalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.medical_store_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalStoreApplication.class, args);
+	}
+
+}
